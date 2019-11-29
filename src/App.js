fbf24f318414c67a34d540a9dee8e4e497a42733
Ben Whitfield <ben.whitfield@ANSGRP-002884.local>
2019-11-29 16:21:02 +0000

    add keyboard drumkit page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import { KeyboardDrumkit } from "./components/keyboardDrumkit/KeyboardDrumkit";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Habitual Linestepper - Landing Page - Again</p>
+        <KeyboardDrumkit />
       </header>
     </div>
   );
