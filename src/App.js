42ba7b28c60068ea6c12bc31ad26166aa9e1cec2
Ben Whitfield <ben.whitfield@ANSGRP-002884.local>
2019-11-29 14:35:47 +0000

    add travis.yml

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Habitual Linestepper</p>
+        <p>Habitual Linestepper - Landing Page</p>
       </header>
     </div>
   );
