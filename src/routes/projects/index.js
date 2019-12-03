dfd990f86f1c14bd52cb1a633d8080341584f441
Ben Whitfield <ben.whitfield@ANSGRP-002884.local>
2019-12-03 10:04:27 +0000

    add tetris and clock projects

--- src/routes/projects/index.js
+++ src/routes/projects/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import KeyboardDrumkit from "../../components/projects/keyboardDrumkit/KeyboardDrumkit";
+import Tetris from "../../components/projects/tetris/components/Tetris";
+import Clock from "../../components/projects/clock/Clock";
 
 const ProjectsRouter = () => (
   <Router>
     <Route exact path="/projects/1" component={KeyboardDrumkit} />
-    {/* <Route path="/projects/2" component={ReactTetris} /> */}
+    <Route path="/projects/2" component={Tetris} />
+    <Route path="/projects/3" component={Clock} />
   </Router>
 );
 
