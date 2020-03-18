d842df3c71912ea66ea1aa34bf32a284a6e3094e
Ben Whitfield <ben.whitfield@ansgrp-002884.home>
2020-03-18 10:50:16 +0000

    remove unused import useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 
 import { AuthContext } from './appConfigs/contextProvider';
