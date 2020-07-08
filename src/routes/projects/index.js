bb1cc162d03b5c2b20a5309a09b2ea5603bf138d
Ben Whitfield <ben.whitfield@ansgrp-002884.home>
2020-07-08 09:59:10 +0100

    remove strava overlay from routes

--- src/routes/projects/index.js
+++ src/routes/projects/index.js
@@ -5,13 +5,13 @@ import AnimatedSwitch from '../../common/animatedSwitch/AnimatedSwitch';
 
 import KeyboardDrumkit from '../../components/projects/keyboardDrumkit/KeyboardDrumkit';
 import Tetris from '../../components/projects/tetris/components/Tetris';
-import StravaOverlay from '../../components/projects/stravaOverlay/StravaOverlay';
+// import StravaOverlay from '../../components/projects/stravaOverlay/StravaOverlay';
 
 const ProjectsRouter = () => (
   <AnimatedSwitch>
     <Route exact path="/projects/drums" component={KeyboardDrumkit} />
     <Route exact path="/projects/tetris" component={Tetris} />
-    <Route exact path="/projects/strava-overlay" component={StravaOverlay} />
+    {/* <Route exact path="/projects/strava-overlay" component={StravaOverlay} /> */}
   </AnimatedSwitch>
 );
 
