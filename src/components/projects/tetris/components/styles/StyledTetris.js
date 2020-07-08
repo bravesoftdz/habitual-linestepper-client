edbd6d01f4f4cb24e8019f241ad1233724c81813
Ben Whitfield <ben.whitfield@ansgrp-002884.home>
2020-07-08 09:20:34 +0100

    update tetris styles for full screen

--- src/components/projects/tetris/components/styles/StyledTetris.js
+++ src/components/projects/tetris/components/styles/StyledTetris.js
@@ -1,9 +1,12 @@
-import styled from "styled-components";
-import bgImage from "../../img/bg.png";
+import styled from 'styled-components';
+import bgImage from '../../img/bg.png';
 
 export const StyledTetrisWrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background: url(${bgImage}) #000;
   background-size: cover;
   overflow: hidden;
@@ -15,7 +18,7 @@ export const StyledTetris = styled.div`
   padding: 40px;
   margin: 0 auto;
   max-width: 900px;
-  height: 100%;
+  min-width: 600px;
 
   aside {
     width: 100%;
