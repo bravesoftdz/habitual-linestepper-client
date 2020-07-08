3ea7b5efe778652e50093e819832a01fcc17b656
Ben Whitfield <ben.whitfield@ansgrp-002884.home>
2020-07-08 09:23:25 +0100

    remove strava overlay project from menu

--- src/components/main/menuPanel/menuConsts.js
+++ src/components/main/menuPanel/menuConsts.js
@@ -1,20 +1,24 @@
 export default {
   0: {
-    title: "PROJECTS",
-    items: ["Tetris", "Drums"],
-    background: "#EFEA5A",
-    color: "hotpink"
+    title: 'PROJECTS',
+    items: [
+      { name: 'Tetris', route: 'tetris' },
+      { name: 'Drums', route: 'drums' },
+      // { name: 'Strava Overlay', route: 'strava-overlay' },
+    ],
+    background: '#EFEA5A',
+    color: 'hotpink',
   },
   1: {
-    title: "BLOG",
+    title: 'BLOG',
     items: [],
-    background: "#13B479",
-    color: "#EFEA5A"
+    background: '#13B479',
+    color: '#EFEA5A',
   },
   2: {
-    title: "PERSONAL",
-    items: ["Daily"],
-    background: "#55769A",
-    color: "#13B479"
-  }
+    title: 'PERSONAL',
+    items: ['Daily'],
+    background: '#55769A',
+    color: '#13B479',
+  },
 };
